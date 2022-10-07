99b30159a295cac9ee114ad790f3701a308bed3c
hhnkong <serenata05@naver.com>
2022-10-07 11:57:20 +0900

    feat: add pages

---
+++ src/pages.tsx
@@ -0,0 +1,17 @@
+export function Home() {
+  return (
+    <main>
+      <h2>Home</h2>
+      <button>About</button>
+    </main>
+  )
+}
+
+export function About() {
+  return (
+    <main>
+      <h2>About</h2>
+      <button>Go Home</button>
+    </main>
+  )
+}
